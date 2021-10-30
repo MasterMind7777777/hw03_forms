35fd9c29a432749ab9306408808e84328aa2a7f1
MasterMind7777777 <go.go.nest.nest@gmail.com>
2021-10-30 12:33:59 +0300

    tests static pages

---
+++ yatube/about/tests/test_urls.py
@@ -0,0 +1,17 @@
+from django.test import Client, TestCase
+
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        # Создаем неавторизованый клиент
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности адреса /page/about/."""
+        response = self.guest_client.get('/page/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_url_uses_correct_template(self):
+        """Проверка шаблона для адреса /page/about/."""
+        response = self.guest_client.get('/page/about/')
+        self.assertTemplateUsed(response, 'staticpages/about.html')
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -30,6 +30,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+
+    def __str__(self):
+        return self.text[:15]
 
     class Meta:
         ordering = ['-pub_date']
